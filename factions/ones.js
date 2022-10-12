81bcbf7f12413840e7b92b42168fb2e4c2cb0b4d
Sundex1 <dominicachilds@gmail.com>
2022-10-12 09:50:56 -0400

    CSB Transfer

--- factions/ones.js
+++ factions/ones.js
@@ -1,15 +1,22 @@
-
-import {xxCount} from './xx.js';
-import {FactionBase} from './faction.js';
+import { xxCount } from "./xx.js";
+import { FactionBase } from "./faction.js";
 
 class OnesFaction extends FactionBase {
   constructor() {
-    super("Ones", ((x) => Math.pow(10, Math.pow(x, xxCount.milestoneReduction))/9));
-    this.challengeReward = Math.floor(Math.pow(this.challenges[0]+this.challenges[1]+this.challenges[2], 1/3))
-    this.challenges = [0,0,0]
+    super(
+      "Ones",
+      (x) => Math.pow(10, Math.pow(x, xxCount.milestoneReduction)) / 9
+    );
+    this.challengeReward = Math.floor(
+      Math.pow(
+        this.challenges[0] + this.challenges[1] + this.challenges[2],
+        1 / 3
+      )
+    );
+    this.challenges = [0, 0, 0];
   }
-  getNextCount(){
-    this.nextCount = this.count+1;
+  get nextCount() {
+    return this.count + 1;
   }
 }
 
